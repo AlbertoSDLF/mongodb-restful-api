e049c4b419630ff43d57d450ec7b74bb34a1fdc8
Alberto Saez de la Fuente <alberto.saezdelafuente@santander.co.uk>
2018-06-15 14:01:58 +0100

    Modifications in controllers

--- lib/controller/errorController.ts
+++ lib/controller/errorController.ts
@@ -7,27 +7,10 @@ import GenericController from "./genericController";
 
 export default class ErrorController extends GenericController {
     public createRoutes(server: Server) {
-        server.on("NotAcceptable", (request: Request, response: Response, error, cb): void => {
-            logger.warn(`${request.id()} => NOT_OK ${HttpStatus.NOT_ACCEPTABLE} ${error.message}`);
-            error.toJSON = function toJSON() {
-                return new CustomResponse(HttpStatus.NOT_ACCEPTABLE, error.message, request);
-            };
-            cb();
-        });
-        server.on("Unauthorized", (request: Request, response: Response, error, cb): void => {
-            logger.warn(`${request.id()} => NOT_OK ${HttpStatus.UNAUTHORIZED} ${error.message}`);
-            error.toJSON = function toJSON() {
-                return CustomResponse.getDefault(HttpStatus.UNAUTHORIZED, request);
-            };
-            cb();
-        });
-        server.on("InvalidCredentials", (request: Request, response: Response, error, cb): void => {
-            logger.warn(`${request.id()} => NOT_OK ${HttpStatus.UNAUTHORIZED} ${error.message}`);
-            error.toJSON = function toJSON() {
-                return new CustomResponse(HttpStatus.UNAUTHORIZED, error.message, request);
-            };
-            cb();
-        });
+        server.on("NotAcceptable", this.defaultHandler(HttpStatus.NOT_ACCEPTABLE));
+        server.on("Unauthorized", this.defaultHandler(HttpStatus.UNAUTHORIZED));
+        server.on("InvalidCredentials", this.defaultHandler(HttpStatus.UNAUTHORIZED));
+        
         server.on("NotFound", (request: Request, response: Response, error, cb): void => {
             logger.warn(`${request.id()} => NOT_OK ${HttpStatus.NOT_FOUND} ${error.message}`);
             error.toJSON = function toJSON() {
@@ -35,10 +18,20 @@ export default class ErrorController extends GenericController {
                     return new CustomResponse(HttpStatus.NOT_FOUND, error.message, request);
                 } else {
                     return CustomResponse.getDefault(HttpStatus.NOT_FOUND, request);
-                };
-            }
+                }
+            };
             cb();
         });
+    }
+
+    private defaultHandler = (httpStatus) => {
+        return (request: Request, response: Response, error, cb): void => {
+            logger.warn(`${request.id()} => NOT_OK ${httpStatus} ${error.message}`);
+            error.toJSON = function toJSON() {
+                return new CustomResponse(httpStatus, error.message, request);
+            };
+            cb();
+        };
     }
 }
 
--- lib/controller/requestLoggingFilter.ts
+++ lib/controller/requestLoggingFilter.ts
@@ -1,6 +1,5 @@
 import { Next, Request, Response } from "restify";
 import { Server } from "restify";
-import * as uuid from "uuid";
 import * as logger from "winston";
 import GenericController from "./genericController";
 
@@ -11,10 +10,9 @@ export default class RequestLoggingFilter extends GenericController {
 
     public createRoutes(server: Server) {
         server.pre((request: Request, response: Response, next: Next) => {
-            const requestId = uuid.v1();
             const ip = request.header("X-Forwarded-For") || request.connection.remoteAddress;
             /* tslint:disable:max-line-length */
-            logger.info(`${requestId} => ${request.method} ${request.getPath()} from ${ip} using ${request.header("User-Agent")}`);
+            logger.info(`${request.id()} => ${request.method} ${request.getPath()} from ${ip} using ${request.header("User-Agent")}`);
             next();
         });
     }
